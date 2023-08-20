c7a568cc6a6811223fa7b9b7eab85fdcf79b159d
omochiumaiumai <shu.nakazato0419@gmail.com>
2023-08-21 00:26:19 +0900

    SWRでデータフェッチするように修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from "react";
-import "./App.css";
+import useSWR from 'swr';
+import './App.css';
 
 function App() {
-  const url = "https://httpstat.us/200?sleep=2000";
-  const headers = { Accept: "application/json" };
+  const url = 'https://httpstat.us/200?sleep=2000';
+  const headers = { Accept: 'application/json' };
 
-  const [status, setStatus] = useState("");
+  const fetcher = (url) =>
+    fetch(url, { headers })
+      .then((res) => res.json())
+      .then((json) => json.description);
+  const { data, error, isLoading } = useSWR(url, fetcher);
+  if (error) return <div>Failed to load.</div>;
+  if (isLoading) return <div>Loading...</div>;
 
-  fetch(url, { headers })
-    .then((res) => res.json())
-    .then((json) => setStatus(json.description));
-
-  return <>{status && <p>Status : {status}</p>}</>;
+  return <div>Status: {data}</div>;
 }
 
 export default App;
